39ce055895a1ec97358d8693c98737c1d38bf320
yoleinerodriguezccs-hash <yoleinerodriguezccs@gmail.com>
2026-08-18 08:12:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+import streamlit as st
+import pandas as pd
 
+st.title("📦 Control de Inventario")
+
+st.write("Sube un archivo Excel para analizar tu inventario.")
+
+archivo = st.file_uploader(
+    "Selecciona tu archivo Excel",
+    type=["xlsx"]
+)
+
+if archivo is not None:
+
+    datos = pd.read_excel(archivo)
+
+    st.subheader("📋 Inventario")
+
+    st.dataframe(datos)
